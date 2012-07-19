cad15f1fc927e4d8b851fc109078c842d58956b3
Akihito Sudo <akihito.s.gm@gmail.com>
2012-07-19 22:08:57 +0900

    ok

---
+++ app/models/about.rb
@@ -0,0 +1,3 @@
+class About < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120719105830) do
+ActiveRecord::Schema.define(:version => 20120719124844) do
+
+  create_table "abouts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "contacts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "menus", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "subscribers", :force => true do |t|
     t.string   "email"
